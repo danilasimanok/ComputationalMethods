df0e4089e82205bc8c2b03dd52466d6857c683a7
Danila Simanok <dsimanok@bk.ru>
2022-09-15 20:22:49 +0300

    regularisation done, examples finished

--- utils/feb24/lu/examples.py
+++ utils/feb24/lu/examples.py
@@ -1,16 +1,46 @@
-import numpy as np
 from ...linear import LinearSystem, hilbert_left_side
+from random import uniform as random
 
-alpha = -1/15
+delta = -1/15
 
 m1_lft = [
 	[1, -1, -1, -1, -1],
-	[alpha, 1, -1, -1, -1],
-	[alpha, 0, 1, -1, -1],
-	[alpha, 0, 0, 1, -1],
-	[alpha, 0, 0, 0, 1]
+	[delta, 1, -1, -1, -1],
+	[delta, 0, 1, -1, -1],
+	[delta, 0, 0, 1, -1],
+	[delta, 0, 0, 0, 1]
 ]
 
 m1_rght = [0, 0, 0, 0, 0]
 
-examples = [LinearSystem(m1_lft, m1_rght)]
+m2_lft = [
+	[1, 2, 4],
+	[3, 8, 14],
+	[2, 6, 13]
+]
+
+m2_rght = [1, 1, 1]
+
+m3_lft = [
+	[-400.6, 199.8],
+	[1198.8, -600.4]
+]
+
+m3_rght = [200, -600]
+
+m4_lft = [
+	[1, 0.99],
+	[0.99, 0.98]
+]
+
+m4_rght = [-0.393, -0.398]
+
+hilbert_rght = [ random(0, 1) for _ in range(5) ]
+
+examples = [
+	LinearSystem(m1_lft, m1_rght),
+	LinearSystem(m2_lft, m2_rght),
+	LinearSystem(m3_lft, m3_rght),
+	LinearSystem(m4_lft, m4_rght),
+	LinearSystem(hilbert_left_side(5), hilbert_rght),
+]
